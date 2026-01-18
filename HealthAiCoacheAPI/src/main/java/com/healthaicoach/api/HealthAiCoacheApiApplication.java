53a64a98bb4db0a7240bc94d7bb948b1705f05cd
baptiste col <baptiste.col2005@gmail.com>
2026-01-18 14:49:31 +0100

    Init Backend

---
+++ HealthAiCoacheAPI/src/main/java/com/healthaicoach/api/HealthAiCoacheApiApplication.java
@@ -0,0 +1,13 @@
+package com.healthaicoach.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthAiCoacheApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthAiCoacheApiApplication.class, args);
+	}
+
+}
